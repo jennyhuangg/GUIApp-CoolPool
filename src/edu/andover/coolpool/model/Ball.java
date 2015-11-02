ce16b78525c4952be94d755f4c3b13bed957a3f0
Eric Lee <elee@andover.edu>
2015-11-02 11:31:39 -0500

    Update Ball.java

--- src/edu/andover/coolpool/model/Ball.java
+++ src/edu/andover/coolpool/model/Ball.java
@@ -5,43 +5,32 @@ import edu.andover.coolpool.view.BallSprite;
 public class Ball {
 	private boolean isPocketed;
 
-	private double xVelocity;
-	private double yVelocity;
+	private double xVelocity; //in CM/s
+	private double yVelocity; //in CM/s
 
-	private final double radius = 2.86;
+	private Point center; //stores Point(centerX, centerY);
 
-	private final double cmToPixel = 37.795;
+	private final double radius = 2.86; //in centimeters
 
-	private BallSprite view;
-
-
-	public Ball(int id) {
-
-		view = new BallSprite(id);
-
-		isPocketed = false;
-
+	public Ball(int x) {
+	    isPocketed = false;
 	}
 
-	public double getCenterX() { return view.getCenterX() / cmToPixel; }
 
-	public double getCenterY() { return view.getCenterY() / cmToPixel; }
+	public double getCenterX() { return center.x; }
+	public double getCenterY() { return center.y; }
 
-	public double setCenterX(double centerX) { view.setCenterX(centerX * cmToPixel); }
-
-	public double setCenterY(double centerY) { view.setCenterY(centerY * cmToPixel); }
-
-	public void setXVelocity(double xVel) { xVelocity = xVel; };
-
-	public void setYVelocity(double yVel) { yVelocity = yVel; };
+	public void setCenterX(double centerX) { center.x = centerX; }
+	public void setCenterY(double centerY) { center.y = centerY; }
+	public void setXVelocity(double xVel) { xVelocity = xVel; }
+	public void setYVelocity(double yVel) { yVelocity = yVel; }
 
 
 	public void slowDown(){
+	    xVelocity -= 1; //deceleration of 1 m/s^2
 
-		xVelocity *= 99/100;
-
-		yVelocity *= 99/100;
-
+	    yVelocity -= 1; //deceleration of 1 m/s^2
 	}
+
 
 }
