06d4dde4618b9e34adeab6f5841eb1c3fbe99443
Pranav Tadikonda <pranavtadikonda@Pranavs-Air.andover.edu>
2015-11-06 12:01:26 -0500

    Added BallSounds Class

---
+++ src/edu/andover/coolpool/view/GameSounds.java
@@ -0,0 +1,11 @@
+package edu.andover.coolpool.view;
+
+import javafx.scene.media.AudioClip;
+
+public class GameSounds {
+	
+	public static final AudioClip BALL_HIT_BALL = new AudioClip("file:res/BallHittingBall.wav");
+	public static final AudioClip CUE_HITTING_BALL = new AudioClip("file:res/PoolCueHittingBall.wav");
+	public static final AudioClip BALL_FALLING_IN_POCKET = new AudioClip("file:res/BallFallingInPocket.wav");
+	
+}
