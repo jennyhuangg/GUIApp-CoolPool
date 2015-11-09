4b07a79dfd1fb63b42eafec0509c5a8b77cb1898
Jenny Huang <jhuang@Jennys-MBP.andover.edu>
2015-11-09 01:16:51 -0500

    Added some cue stick things

--- src/edu/andover/coolpool/controller/CueStickController.java
+++ src/edu/andover/coolpool/controller/CueStickController.java
@@ -1,24 +1,19 @@
 package edu.andover.coolpool.controller;
 
 import edu.andover.coolpool.model.CueStick;
-import edu.andover.coolpool.model.PoolBoard;
 import edu.andover.coolpool.view.PoolBoardView;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
 public class CueStickController {
 	
 	public void addMouseEventHandler(PoolBoardView pbv, CueStick cueStick) {
 		Rectangle r = pbv.getRectangle();
-	    r.setOnMouseMoved(new EventHandler<MouseEvent>() {
+	    r.addEventHandler(MouseEvent.MOUSE_MOVED, new EventHandler<MouseEvent>() {
 	    	@Override
 	    	public void handle(MouseEvent me) {
-	    		cueStick.setCueStickLocation(me.getX(), me.getY());
+	    		cueStick.setCueStickLocation(me.getSceneX(), me.getSceneY());
 	    	}
 	    });
 	}
