fb0c8b96b25e3f47bb52ed17bf8ab329673ebeb8
Jenny Huang <jhuang@Jennys-MBP.andover.edu>
2015-11-17 20:31:08 -0500

    Finished ball methods

--- src/edu/andover/coolpool/model/tests/BallTest.java
+++ src/edu/andover/coolpool/model/tests/BallTest.java
@@ -20,7 +20,7 @@ public class BallTest {
 		double pocketedBallVelocity = 0.0;
 		
 		boolean check = b.getXVelocity() == pocketedBallVelocity;
-		assertTrue("X Velocity is not 0.", check);
+		assertTrue("Failed to set X Velocity to 0.", check);
 	}	
 	
 	@Test
@@ -30,10 +30,35 @@ public class BallTest {
 		double pocketedBallVelocity = 0.0;
 		
 		boolean check = b.getYVelocity() == pocketedBallVelocity;
-		assertTrue("Y Velocity is not 0.", check);
+		assertTrue("Failed to set Y Velocity to 0.", check);
 	}
 	
-	// TODO: setPocketed centerX should be 0
-	// TODO: setPocketed centerX should be 0
-	// TODO: unpocketed should negate isPocketed.
+	@Test
+	public void setPocketedShouldGivePocketedBall0XCenterPos() {
+		Ball b = new Ball(2);
+		b.setPocketed();
+		double pocketedBallXPosition = 0.0;
+		
+		boolean check = b.getYVelocity() == pocketedBallXPosition;
+		assertTrue("Failed to set center x position to 0.", check);
+	}
+	
+	@Test
+	public void setPocketedShouldGivePocketedBall0YCenterPos() {
+		Ball b = new Ball(2);
+		b.setPocketed();
+		double pocketedBallYPosition = 0.0;
+		
+		boolean check = b.getCenterY() == pocketedBallYPosition;
+		assertTrue("Failed to set center y position to 0.", check);
+	}
+	
+	@Test
+	public void unPocketedShouldNegateIsPocketed() {
+		Ball b = new Ball(2);
+		b.setPocketed();
+		b.unpocket();
+		boolean isPocketed = b.isPocketed();
+		assertFalse("Failed to unpocket ball", isPocketed);
+	}
 }
